6d7a9d8888a506a119c754a551d4d4ba33edc488
vishal <jsvishal2014@gmail.com>
2023-07-23 09:57:07 +0530

    database

--- dist/providers/Database.js
+++ dist/providers/Database.js
@@ -14,6 +14,7 @@ class Database {
     // Initialize your database pool
     static init() {
         const dsn = Locals_1.default.config().mongooseUrl;
+        console.log(dsn);
         const options = { useNewUrlParser: true, useUnifiedTopology: true };
         mongoose.Promise = bluebird;
         // mongoose.set('useCreateIndex', true);
